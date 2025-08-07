542072ad258c18d5f270769c25c35b7ff10b4332
Reinel Gueco <venvengueco@gmail.com>
2025-08-07 11:21:11 +0800

    fixed viewport

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./frontend/**/*.{html,js}",
+    "./src/**/*.{css,js}",
+    "./*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
