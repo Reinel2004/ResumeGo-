2bbbc2a348f52fef2f9a06656cae14f40a7e7f59
Reinel Gueco <venvengueco@gmail.com>
2025-09-29 11:22:37 +0800

    initial commit

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
     HOST: process.env.DB_HOST,
     USER: process.env.DB_USER,
     PASSWORD: process.env.DB_PASSWORD,
-    DB: process.env.DB_NAME || "resume_generator",
+    DB: process.env.DB_NAME,
     dialect: "mysql",
     pool: {
         max: 5,
